0f101606bb4cd37b1284ac4a53ac0560dfc72b09
Patrick Richter <patrick-richter@posteo.de>
2025-01-26 23:38:19 +0100

    Strange bug appears when sending messages to all joined rooms: An error message is raised that states that the room does not exist when the script was executed before without sending messages.

--- join_and_leave_rooms.py
+++ join_and_leave_rooms.py
@@ -10,7 +10,7 @@ https://github.com/gratach/create-matrix-nio-bot-dir/blob/ac12df13a8203c62894012
 
 import asyncio
 from pathlib import Path
-from nio import AsyncClient, AsyncClientConfig, JoinError, RoomMemberEvent
+from nio import AsyncClient, AsyncClientConfig, JoinError, RoomMemberEvent, InviteMemberEvent
 from json import load
 from aioconsole import ainput
 
@@ -44,18 +44,18 @@ async def main():
     client.user_id = login_info["user_id"]
     client.access_token = login_info["access_token"]
 
+    # Load the stored sync tokens
+    client.load_store()
+
+    # Upload keys if necessary
+    if client.should_upload_keys:
+        await client.keys_upload()
+
     # Syncronize with the server
-    resp = await client.sync()    # --. When this line gets moved here, the client.invited_rooms is empty even though the client is invited to rooms
-                                  #   |
-    # Load the stored sync tokens #   |
-    client.load_store()           #   |
-                                  #   |
-    # Upload keys if necessary    #   |
-    if client.should_upload_keys: #   |
-        await client.keys_upload()#   |
-                                  # <-*    
-    # Iterate over all invitations up to this point
-    # For some reason the client.invited_rooms is empty
+    resp = await client.sync()
+
+    # Iterate over all invitations that were received since the last time the bot was running
+    # Invatations that were received before or during the last run are not listed
     for room_id in client.invited_rooms.keys():
         try:
             # Join the room
@@ -75,6 +75,50 @@ async def main():
         if len(members) == 1:
             await client.room_leave(room_id)
 
-    await client.close()
+    # The callback functions to handle invites and member events
+
+    async def on_invite(room, event):
+        # If you are invited to a room
+        if event.state_key == client.user_id:
+            # Join the room
+            try:
+                resp = await client.join(room.room_id)
+                if isinstance(resp, JoinError):
+                    raise Exception("Failed to join room")
+            except Exception as e:
+                # Leave the room if joining failed
+                resp = await client.room_leave(room.room_id)
+    async def on_member_event(room, event):
+        # If someone else leaves or is banned from the room
+        if (event.membership == "leave" or event.membership == "ban") and event.state_key != client.user_id:
+            # Get all members in the room
+            members = (await client.joined_members(room.room_id)).members
+            # If I am the only member, leave the room
+            if len(members) == 1:
+                await client.room_leave(room.room_id)
+
+    # Add the callbacks
+    client.add_event_callback(on_invite, (InviteMemberEvent,))
+    client.add_event_callback(on_member_event, (RoomMemberEvent,))
+
+    # Function to close the client with the command line
+    async def wait_for_close():
+        await ainput("Press enter to close the client")
+        await client.close()
+        print("It will take some time to close the client. Please wait...")
+    
+    # Function to sync the client forever
+    async def sync_forever():
+        try:
+            await client.sync_forever(timeout=30000)
+        except Exception as e:
+            print(e)
+            await client.close()
+
+    # Sync until the client is closed
+    await asyncio.gather(
+        sync_forever(),
+        wait_for_close()
+        )
 
 asyncio.run(main())
--- message_to_all_rooms.py
+++ message_to_all_rooms.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 """
-This script sends a message to all rooms the bot is in.
+This script asks the user if it should send a message to all joined rooms.
 
-The botdir folder contains the login.json file with the login information (homeserver, user_id, device_id, access_token) and the store folder of the AsyncClient.
-It can be prepared and verified with the create_bot_dir.py script:
+To work properly, the botdir folder must contain a login.json file with the login information (homeserver, user_id, device_id, access_token) and the store folder of the AsyncClient.
+This folder can be prepared and verified with the create_bot_dir.py script:
 https://github.com/gratach/create-matrix-nio-bot-dir/blob/ac12df13a8203c6289401296a7dcebae5da8842e/create_bot_dir.py
+
+A verry strange bug can be observed:
+When this script is executed and the user chooses not to send a message, something gets broken.
+When the script is executed again and the user chooses to send a message, the LocalProtocolError "No such room with id ... found" is raised for all rooms.
+Now if something gets written in one of the rooms, the error disappears for this room and the script works as expected.
+The script can be executed multiple times without any problems as long as the user chooses to send a message.
+When the user chooses not to send a message, the error appears again on the next execution.
 """
 
 import asyncio
 from pathlib import Path
-from nio import AsyncClient, AsyncClientConfig
+from nio import AsyncClient, AsyncClientConfig, LocalProtocolError
 from json import load
 
 BOTDIR = Path(__file__).parent / "botdir"
@@ -55,17 +62,24 @@ async def main():
     # Get all joined rooms
     rooms = (await client.joined_rooms()).rooms
 
-    # Send message to all joined rooms
-    # Strange error: In some cases, the room_send method raises an localProtocolError "No such room with id ... found"
-    for room_id in rooms:
-        await client.room_send(
-            room_id,
-            "m.room.message",
-            {
-                "msgtype": "m.text",
-                "body": "Hi to all rooms",
-            },
-            ignore_unverified_devices=True,
-        )
+    if input("Do you want to send messages to all rooms? (y/n) ") == "y":
+
+        # Send message to all joined rooms
+        for room_id in rooms:
+            try:
+                await client.room_send(
+                    room_id,
+                    "m.room.message",
+                    {
+                        "msgtype": "m.text",
+                        "body": "Hi to all rooms!",
+                    },
+                    ignore_unverified_devices=True,
+                )
+            except LocalProtocolError as e:
+                print("No message could be sent to room", room_id)
+                print("Error: " + " ".join(e.args))
+    
+    await client.close()
 
 asyncio.run(main())
